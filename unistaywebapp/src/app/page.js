752f440658f7c1f441beae80cdac4aa1d914ce33
Gustavo <gumarinho090306@gmail.com>
2023-09-05 23:04:30 -0300

    Icon styling

--- unistaywebapp/src/app/page.js
+++ unistaywebapp/src/app/page.js
@@ -1,13 +1,15 @@
-import { Home as HomeIcon } from "@mui/icons-material";
+import { LanguageOutlined as Lang } from "@mui/icons-material";
+import { AccountCircleOutlined as Profile } from "@mui/icons-material";
 
-function CustomIcon() {
-  return <HomeIcon className="text-red-950" />
+function CustomIcon(Prop) {
+  return <Prop className="text-bodyemphasized text-2xl" />
 }
 
 export default function Pagey() {
   return (
-    <div className="bg-background-light dark:bg-background">
-      <CustomIcon />
+    <div className="bg-background-light dark:bg-background w-full h-[100vh]">
+      {CustomIcon(Lang)}
+      {CustomIcon(Profile)}
     </div>
   )
 }
