210a796f97b04947ede086ae9bc7c354ae8e2556
Gustavo <gumarinho090306@gmail.com>
2023-09-09 21:46:53 -0300

    Accommodation cards have been normalized

--- unistaywebapp/src/app/accommodationItem.js
+++ unistaywebapp/src/app/accommodationItem.js
@@ -11,10 +11,11 @@ export default function AccommodationItem(props) {
     const rating = props.rating
     const className = props.className;
     return(
-        <div className={`w-[40vw] min-w-[120px] md:w-[15vw] max-w-[200px] ${className}`}>
-            <figure className="w-[40vw] h-[40vw] min-w-[120px] min-h-[120px] md:w-[15vw] md:h-[15vw] max-w-[200px] max-h-[200px] bg-cover bg-center rounded-xl" style={{backgroundImage: `url(${imageUrl})`}}></figure>
+        <div className={`my-5 w-[calc(33vw+(2*0.75rem))] md:w-[calc(15vw+(2*0.75rem))] max-w-[calc(200px+(2*0.75rem))] bg-gray rounded-xl p-3 ${className}`}>
+            <figure className="w-[33vw] h-[33vw] md:w-[15vw] md:h-[15vw] max-w-[200px] max-h-[200px] bg-cover bg-center rounded-xl" style={{backgroundImage: `url(${imageUrl})`}}></figure>
             <div>
-                <Label text={location} icon={Loc} order={true} measures="my-4" textProperties="text-xs" />
+                <Label text={location} icon={Loc} order={true} measures="my-4 sm:max-w-[initial] hidden sm:flex" textProperties="text-xs" />
+                <p className="block sm:hidden my-4">{location}</p>
                 <p>{rent}<span className="">/month</span></p>
                 <Label text={rating} icon={Star} order={false} measures="mt-4" textProperties="text-xs" />
                 
