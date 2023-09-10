ba41dad8277bbc918f0c3358896789a1c355c90d
Gustavo <gumarinho090306@gmail.com>
2023-09-10 00:03:55 -0300

    Reconfiguring the page container size

--- unistaywebapp/src/app/navBar.js
+++ unistaywebapp/src/app/navBar.js
@@ -4,7 +4,7 @@ import SearchBar from "./searchBar"
 
 export default function NavBar() {
     return (
-        <nav className="flex items-center justify-between dark:text-body-dark-emphasized">
+        <nav className="flex items-center justify-between dark:text-body-dark-emphasized z-10 relative">
             <Logo />
             <SearchBar menu={false} />
             <Options />
--- unistaywebapp/src/app/page.js
+++ unistaywebapp/src/app/page.js
@@ -3,7 +3,7 @@ import NavBar from "./navBar";
 
 export default function Page() {
   return (
-    <div className="mx-auto container relative lg:max-w-[1024px] max-h-screen overflow-hidden p-5 text-xs font-einaReg dark:text-body-dark text-body-light">
+    <div className="mx-auto container relative sm:max-w-[516px] md:max-w-[768px] lg:max-w-[1024px] max-h-screen overflow-hidden p-5 text-xs font-einaReg dark:text-body-dark text-body-light">
       <NavBar />
       <Picker />
     </div>
