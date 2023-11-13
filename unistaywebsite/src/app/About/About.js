fb31ee8e2ff635fc945afc6e78a24eb0c0de020c
Gustavo <gumarinho090306@gmail.com>
2023-11-12 21:59:51 -0300

    Fetching background image from api.unistay.studio

--- unistaywebsite/src/app/About/About.js
+++ unistaywebsite/src/app/About/About.js
@@ -6,9 +6,14 @@ const About = () => {
     return (
         <div className="flex flex-col items-center gap-4">
             <h2 className="font-einaSemi max-w-[300px] sm:max-w-[200px]">We are all about helping you to find your unistay</h2>
-            <p className="max-w-[400px]">Create an account. Search locations, universities, accommodation types and more. Find your place.</p>
+            <p className="max-w-[400px] font-einaReg">Create an account. Search locations, universities, accommodation types and more. Find your place.</p>
             <Button text='Find' onClick={() => {}} />
-            <Image src='/aboutmobile.png' className='mt-10 max-w-[300px]' width={800} height={1822} />
+            <div className='sm:hidden'>
+                <Image src='/aboutmobile.png' className='mt-10 max-w-[300px]' width={800} height={1822} />
+            </div>
+            <div className='hidden sm:block'>
+                <Image src='/aboutlg.png' className='mt-10 max-w-[600px]' width={800} height={1822} />
+            </div>
         </div>
     )
 }
--- unistaywebsite/src/app/page.js
+++ unistaywebsite/src/app/page.js
@@ -6,13 +6,14 @@ import About from './About/About'
 export default function Home() {
   return (
     <main>
-      <section className='bg-gradient p-8 font-einaReg text-sm overflow-hidden'>
+      <section className='bg-gradient relative p-8 font-einaReg text-sm overflow-hidden'>
         <section className='mx-auto container relative sm:max-w-[516px] md:max-w-[768px] lg:max-w-[1024px]'>
           <Navbar />
           <Hero />
         </section>
+        <div className='h-32 bg-gradient-to-t absolute bottom-0 w-screen from-background to-background/0'></div>
       </section>
-      <section className='text-center bg-background'>
+      <section className='text-center p-8'>
         <About />
       </section>
     </main>
