414b792029177d25413d0e7e0b96efb8078bd77e
Gustavo <gumarinho090306@gmail.com>
2023-11-21 14:51:02 -0300

    Update SearchOutlined icon size in PlaceholderInput component

--- unistaywebsite/src/app/GeneralUse/PlaceholderInput.js
+++ unistaywebsite/src/app/GeneralUse/PlaceholderInput.js
@@ -4,7 +4,7 @@ function PlaceholderInput({placeholder, onClick}) {
   return (
     <div>
       <div className='flex items-center bg-gray border border-slate-500 px-4 py-2 rounded gap-3'>
-            <SearchOutlined className='w-3 h-3' />
+            <SearchOutlined fontSize="small" />
             <button className='outline-none placeholder:text-body-dark-emphasized bg-transparent' onClick={onClick}>{placeholder}</button>
         </div>
     </div>
