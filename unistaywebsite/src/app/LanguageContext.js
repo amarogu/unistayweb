f855db4d3f01a94f58e14f645413ce5b7fef5ca3
Gustavo <gumarinho090306@gmail.com>
2023-11-23 19:21:05 -0300

    Update language to Portuguese in LanguageProvider

--- unistaywebsite/src/app/LanguageContext.js
+++ unistaywebsite/src/app/LanguageContext.js
@@ -4,7 +4,7 @@ import React from 'react';
 const LanguageContext = React.createContext();
 
 export const LanguageProvider = ({ children }) => {
-  const [language, setLanguage] = React.useState('English');
+  const [language, setLanguage] = React.useState('Portuguese');
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
